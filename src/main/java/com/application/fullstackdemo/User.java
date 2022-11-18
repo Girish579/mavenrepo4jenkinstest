86562db78f7252e61e761f3328ee2dff1f5d3e17
SHIVAKUMAR POTHARAM <keerthi.k25oct@gmail.com>
2022-11-18 10:41:53 +0530

    Updated User.java

--- src/main/java/com/application/fullstackdemo/User.java
+++ src/main/java/com/application/fullstackdemo/User.java
@@ -39,6 +39,39 @@ public void checkbal()
 System.out.println("balance="+bal);
 }
 }
+class Loanacc implements Account
+{
+int accno;
+String accname;
+double bal;
+Loanacc(int accno,String accname,double bal)
+{
+this.accno=accno;
+this.accname=accname;
+this.bal=bal;
+}
+public void withdraw(double amt)
+{
+bal=bal+amt;
+System.out.println("amount withdrawn="+amt);
+}
+public void deposit(double amt)
+{
+if(bal>=amt)
+{
+bal=bal-amt;
+System.out.println("Amount deposited="+amt);
+}
+else
+{
+System.out.println("Do not pay more than loan amount");
+}
+}
+public void checkbal()
+{
+System.out.println("balance="+bal);
+}
+}
 class Accmanager
 {
 public static Account createacc(int accno,String accname,double bal,String type)
@@ -49,7 +82,7 @@ return new Savingsacc(accno,accname,bal);
 }
 else
 {
-	return null;
+return new Loanacc(accno,accname,bal);
 }
 }
 }
@@ -57,13 +90,20 @@ class User
 {
 public static void main(String args[])
 {
-Account a1=Accmanager.createacc(1234,"abc",20000.00,"savings"); //we can create reference variable for interface but we can't create object for interface
+Account a1=Accmanager.createacc(1234,"abc",20000.00,"savings"); //we can create reference variable for interface, but we cat create object.
 System.out.println("For savings account");
 a1.checkbal();
 a1.withdraw(5000);
 a1.checkbal();
 a1.deposit(6000);
 a1.checkbal();
+Account a2=Accmanager.createacc(1234,"abc",20000.00,"loans");
+System.out.println("\nFor loan account");
+a2.checkbal();
+a2.withdraw(5000);
+a2.checkbal();
+a2.deposit(6000);
+a2.checkbal();
 }
 }
     
